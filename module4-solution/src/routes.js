c00d0651b6ed403b9fd42a6a0dfc1dd1a2b3ea2a
toan2013 <toan.d.le3@gmail.com>
2016-12-17 23:22:23 -0800

    rm  root directory from access path to html

--- module4-solution/src/routes.js
+++ module4-solution/src/routes.js
@@ -21,7 +21,7 @@
 
       .state('categories', {
         url: '/categories',
-        templateUrl: '/src/menuapp/templates/main.categories.html',
+        templateUrl: 'src/menuapp/templates/main.categories.html',
         controller: 'CategoriesController as categoryList',
         resolve: {
           categories: ['MenuDataService', function(MenuDataService) {
@@ -33,7 +33,7 @@
 
       .state('items', {
         url: '/categories/{categoryShortName}',
-        templateUrl: '/src/menuapp/templates/main.menu.items.html',
+        templateUrl: 'src/menuapp/templates/main.menu.items.html',
         controller: 'MenuItemController as menuItemList',
         resolve: {
           menuItems: ['$stateParams','MenuDataService',
